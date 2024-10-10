6568b7f5ed2901b1e0fd822f08f9ee43c62e2764
Tiago <murtinho@TiagoLaptop>
2024-10-10 16:07:54 +0100

    feat(DEV-1): Inserir logo na janela do navegador.

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,9 +4,7 @@
 <meta content="" name="description">
 <meta content="" name="keywords">
 
-<!-- Favicons -->
-<link href="assets/img/favicon.png" rel="icon">
-<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link rel="icon" href="{{ asset('assets/img/miniLogo.png') }}" type="image/png">
 
 <!-- Google Fonts -->
 <link href="https://fonts.gstatic.com" rel="preconnect">
