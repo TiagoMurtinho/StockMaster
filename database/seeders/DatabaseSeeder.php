ee15c8af7806b90230aa97e33aca90935bc71c07
Tiago <murtinho@TiagoLaptop>
2024-10-16 17:13:41 +0100

    fix(main): Término do projeto.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(TipoPaleteSeeder::class);
         $this->call(TipoDocumentoSeeder::class);
-        $this->call(ClienteSeeder::class);
-        $this->call(ArtigoSeeder::class);
         $this->call(ArmazemSeeder::class);
         $this->call(TaxaSeeder::class);
-        $this->call(DocumentoTipoUmSeeder::class);
-        $this->call(LinhaDocumentoTipoUmSeeder::class);
-        $this->call(PaleteSeeder::class);
-        $this->call(DocumentoTipoDoisSeeder::class);
-        $this->call(LinhaDocumentoTipoDoisSeeder::class);
-        $this->call(DocumentoTipoTresSeeder::class);
-        $this->call(LinhaDocumentoTipoTresSeeder::class);
-        $this->call(DocumentoTipoQuatroSeeder::class);
-        $this->call(LinhaDocumentoTipoQuaatroSeeder::class);
-        $this->call(DocumentoTipoCincoSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,32 +12,18 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run()
+    public function run(): void
     {
-        $faker = Faker::create();
 
         $user = User::create([
-            'name' => 'teste2',
-            'email' => 'teste2@gmail.com',
-            'contacto' => '123456789',
-            'salario' => 3000,
-            'password' => bcrypt('12345678'),
+            'name' => 'admin',
+            'email' => 'admin@exemple.com',
+            'password' => bcrypt('password'),
         ]);
 
         $role = Role::firstOrCreate(['name' => 'Admin']);
 
         $user->assignRole($role);
 
-       /* for ($i = 0; $i < 50; $i++) {
-            DB::table('users')->insert([
-                'name' => $faker->name,
-                'email' => $faker->unique()->safeEmail,
-                'contacto' => $faker->phoneNumber,
-                'salario' => $faker->numberBetween('0', '5000'),
-                'password' => bcrypt('password'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }*/
     }
 }
